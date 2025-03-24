35def0e9cb4949e1edc1b8ea9337f08f7439e30d
Rangana Pradeep <ranganapradeepgunawardhana@gmail.com>
2025-03-25 01:41:49 +0530

    Ready For Testing

--- Client_Frontend/src/Pages/Contact.jsx
+++ Client_Frontend/src/Pages/Contact.jsx
@@ -47,7 +47,6 @@ export default function Contact() {
           setTimeout(() => {
 
             setFormData({
-        
               name: "",
               email: "",
               phoneNumber: "",
