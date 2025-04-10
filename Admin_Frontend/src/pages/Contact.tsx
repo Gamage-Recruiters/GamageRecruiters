4a22d69ed235fe67ac97e05d571d5860e4ef9233
IT22025180 <it22025180@my.sliit.lk>
2025-04-10 12:13:24 +0530

    Contact delete funct integ vanuja

--- Admin_Frontend/src/pages/Contact.tsx
+++ Admin_Frontend/src/pages/Contact.tsx
@@ -30,6 +30,18 @@ const Contact = () => {
         return matchesSearch && matchesStatus;
     });
 
+    const contactDelete = (id) => {
+        Axios.delete(`http://localhost:5000/api/contact/deleteinquiry/${id}`)
+            .then(response => {
+                console.log('Contact deleted successfully');
+                setContacts(prevData => prevData.filter(cont => cont.id !== id));
+            })
+            .catch(error => {
+                console.error('Error deleting contact:', error);
+            });
+    };
+
+
     return (
         <div className="space-y-6">
             <div className="flex justify-between items-center">
@@ -96,13 +108,7 @@ const Contact = () => {
                                     </td>
                                     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                         <div className="flex justify-end space-x-2">
-                                            <Link to={`/view-job/${cont.id}`} className="text-blue-600 hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-300">
-                                                <Eye size={18} />
-                                            </Link>
-                                            <Link to={`/jobss/edit/${cont.id}`} className="text-yellow-600 hover:text-yellow-900 dark:text-yellow-400 dark:hover:text-yellow-300">
-                                                <Edit size={18} />
-                                            </Link>
-                                            <button onClick={() => {/* Delete handling */ }} className="text-red-600 hover:text-red-900 dark:text-red-400 dark:hover:text-red-300">
+                                            <button onClick={() => contactDelete(cont.id)} className="text-red-600 hover:text-red-900 dark:text-red-400 dark:hover:text-red-300">
                                                 <Trash2 size={18} />
                                             </button>
                                         </div>
