9637633123214e476d1e0ac3739829d0c2fae67a
Suchith Sandunika <esuchith@gmail.com>
2025-04-12 10:22:42 +0530

    updated protectedRoute code

--- Client_Frontend/src/protected/ProtectedRoute.jsx
+++ Client_Frontend/src/protected/ProtectedRoute.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import handleToken from '../scripts/handleToken';
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('AccessToken');
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  const token = localStorage.getItem('AccessToken');
+  // return isAuthenticated ? children : <Navigate to="/login" />;
+  if (token) {
+    if(!handleToken(token)) {
+      console.log('Token is not valid');
+      return <Navigate to="/login" />;
+    } else {
+      console.log('Token Authentication Successfull');
+      return children;
+    }
+  } else {
+    return <Navigate to="/login" />;
+  }
 }
 
 export default ProtectedRoute;
