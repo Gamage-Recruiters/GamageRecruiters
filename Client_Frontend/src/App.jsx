f5159283c0b7bdbc642981d01dcafdf8c9feea88
Damsiha Kumarasinghe <damsihak@gmail.com>
2025-07-14 15:38:23 +0530

    Remove WindowOnClose from App.jsx
    The WindowOnClose was clearing the local storage upon closing of a window or a refresh causing to the deletion of the JWT token in cookies.

--- Client_Frontend/src/App.jsx
+++ Client_Frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <Router>
       <ScrollToTop />
-      <WindowOnClose/>
+      {/* <WindowOnClose/> */}
       <div className="flex flex-col min-h-screen">
         <Toaster position="top-center" />
         <Navbar />
