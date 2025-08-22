6119b65af99d384b4f940d93b6c03a9f782bd3b0
Damsiha Kumarasinghe <damsihak@gmail.com>
2025-08-22 21:14:57 +0530

    Gnext logo update home page

--- Client_Frontend/src/components/Patners/TestimonialsSection.jsx
+++ Client_Frontend/src/components/Patners/TestimonialsSection.jsx
@@ -15,7 +15,7 @@ const testimonials = [
   {
     id: 2,
     company: "GNEXT",
-    logo: "https://i.ibb.co/pvn864CX/image.png",
+    logo: "https://i.ibb.co/yFwBn1mQ/Whats-App-Image-2025-08-21-at-09-23-24.jpg",
     quote: "Your recruitment service was instrumental in fulfilling our urgent and critical hiring needs. It allowed us to discreetly recruit for senior positions without alerting the market, ensuring confidentiality throughout the process. Additionally, your ability to source qualified candidates with specialized expertise in our specific industry was invaluable. This tailored approach not only saved us time but also ensured that we secured the right talent for key roles.",
     author: "Amali Rathnapala",
     position: "Manager HR-GNEXT",
